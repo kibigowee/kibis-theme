61f581008bce8d2e31c96c48718747bbf963793a
kibi <69326919+kibigowee@users.noreply.github.com>
2023-11-10 14:36:24 -0800

    Initial commit

---
+++ content.js
@@ -0,0 +1,40 @@
+let chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(){}|:<>*-+";
+
+setInterval(runProgram,250);
+applyStoredChanges();  
+
+function runProgram(){
+    applyStoredChanges();
+    titles = document.getElementsByClassName("lesson-title");
+    for (let i = 0; i < titles.length; i++) {
+        titles[i].innerText=scramble(titles[i].innerText);
+    }
+}
+
+function scramble(str){
+    let newStr = "";
+    for (let i = 0; i < str.length; i++) {
+        newStr += chars.charAt(Math.round(Math.random()*chars.length));
+    }
+    return newStr;
+}
+
+//augment the animation of the icons based on the two colors chosen
+//set property:
+function applyStoredChanges() {
+    // Retrieve the stored parameters from localStorage
+    const storedParams = window.localStorage.getItem("myExtensionParams");
+    console.log("Stored parameters:" + storedParams);
+    if (storedParams) {
+        // Parse the stored parameters as JSON
+        const userPreferences = JSON.parse(storedParams);
+        console.log("found something");
+        
+        // Apply the stored changes to the webpage
+        document.documentElement.style.setProperty('--start-color-unfinished', userPreferences.unfinStartCol);
+        document.documentElement.style.setProperty('--end-color-unfinished', userPreferences.unfinEndCol);
+        document.documentElement.style.setProperty('--start-color-unopened', userPreferences.unopenStartCol);
+        document.documentElement.style.setProperty('--end-color-unopened', userPreferences.unopenEndCol);
+
+    }
+}
---
+++ popup.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const changeButton = document.getElementById("saveChanges");
+  
+    changeButton.addEventListener("click", function () {
+        // Define the parameters you want to pass to the content script and store them in localStorage
+        const x = {
+          unfinStartCol: (document.getElementById("first-unsubmit").value + ""),
+          unfinEndCol: (document.getElementById("second-unsubmit").value + ""),
+          unopenStartCol: (document.getElementById("first-unopen").value + ""),
+          unopenEndCol: (document.getElementById("second-unopen").value + "")
+        };
+        
+        chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+            const activeTab = tabs[0];
+            chrome.scripting.executeScript({
+              target: { tabId: activeTab.id },
+              function: (params) => {
+                localStorage.setItem("myExtensionParams", params);
+              },
+              args: [JSON.stringify(x)]
+            });
+        });
+    });
+});
